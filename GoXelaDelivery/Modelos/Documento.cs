558dc856b5a03414e7c4fab361eb08f46f9da768
deglyya-hub <deglyya@gmail.com>
2026-09-05 14:09:51 -0600

    Feat: Creacion clase documento
    Se agregó clase hija Documento (hereda de Paquete)

---
+++ GoXelaDelivery/Modelos/Documento.cs
@@ -0,0 +1,22 @@
+﻿class Documento : Paquete
+{
+    public Documento(int codigoCons, string descripcionCons, double pesoCons, double valorCons, string origenCons, string destinoCons)
+        : base(codigoCons, descripcionCons, pesoCons, valorCons, origenCons, destinoCons)
+    {
+
+    }
+    public Documento(int codigoCons, string descripcionCons, double pesoCons)
+       : base(codigoCons, descripcionCons, pesoCons)
+    {
+    }
+    public override double CalcularTarifa(double distancia, string tipoServicio)
+    {
+        double tarifaBase = distancia;
+        double recargoServicio = UtilidadesTarifa.RecargoPorServicio(tipoServicio);
+        return tarifaBase * (1 + recargoServicio);
+    }
+    public override bool RequiereCondicionEspecial()
+    {
+        return false;
+    }
+}
