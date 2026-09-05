678a05f7f98be4ba83960fb77a9e3a5074b4801a
deglyya-hub <deglyya@gmail.com>
2026-09-05 13:30:52 -0600

    feat: Creacion UtilidadesTarifa
    Para calcular recargos por el tipo de servicio: (normal, prioritario, urgente)

---
+++ GoXelaDelivery/Modelos/UtilidadesTarifa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+static class UtilidadesTarifa
+{
+    // Recargo por tipo de servicio: Normal 0%, Prioritario +25%, Urgente +50%
+    public static double RecargoPorServicio(string tipoServicio)
+    {
+        if (string.IsNullOrWhiteSpace(tipoServicio))
+            return 0;
+
+        if (tipoServicio.Equals("Prioritario", StringComparison.OrdinalIgnoreCase))
+            return 0.25;
+
+        if (tipoServicio.Equals("Urgente", StringComparison.OrdinalIgnoreCase))
+            return 0.50;
+
+        return 0;
+    }
+}
